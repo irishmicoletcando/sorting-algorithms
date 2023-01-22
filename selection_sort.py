4922b2167de5944b6f8c05d4f22875d541ebd7b6
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-22 12:53:39 +0800

    Rename function
    Change sort function to selection_sort function to be more specific

--- selection_sort.py
+++ selection_sort.py
@@ -1,4 +1,4 @@
-def sort(nums):
+def selection_sort(nums):
   # loop until index 9
   for i in range(len(nums)- 1):
     minpos = i # holds the minimum position
@@ -18,5 +18,5 @@ def sort(nums):
 
 
 nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
-sort(nums)
+selection_sort(nums)
 print(nums)
