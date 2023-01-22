e637d1e78c70bcb7e6118d1c9626297d1a40e2d5
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-22 12:23:41 +0800

    Replace 9 and 10 with length of nums array

--- selection_sort.py
+++ selection_sort.py
@@ -1,8 +1,8 @@
 def sort(nums):
   # loop until index 9
-  for i in range(9):
+  for i in range(len(nums)- 1):
     minpos = i # holds the minimum position
     # loop for minpos
-    for j in range(i, 10):
+    for j in range(i, len(nums)):
 
 nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
