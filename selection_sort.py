aa57f4390641425f642b3ce5d951326a3ed73446
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-22 12:22:54 +0800

    Add loop for minimum position

--- selection_sort.py
+++ selection_sort.py
@@ -2,5 +2,7 @@ def sort(nums):
   # loop until index 9
   for i in range(9):
     minpos = i # holds the minimum position
+    # loop for minpos
+    for j in range(i, 10):
 
 nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
