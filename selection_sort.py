80cedf5167aff059fe8d919d1894ce1bc245d593
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-22 12:40:02 +0800

    Print nums array every loop

--- selection_sort.py
+++ selection_sort.py
@@ -12,5 +12,10 @@ def sort(nums):
     temp = nums[i]
     nums[i] = nums[minpos]
     nums[minpos] = temp
+    
+    # printing every loop
+    print(nums)
 
+    
 nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
+sort(nums)
