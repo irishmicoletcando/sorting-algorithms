40010f32cff6ff1229e9d65361601928e3d5de9b
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-22 13:17:11 +0800

    Implement an inner loop to swap elements

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,12 @@
 def bubble_sort(nums):
   # outer loop
   for i in range(len(nums) - 1, 0, -1):
-    
+    # inner loop
+    for j in range(i):
+      if nums[j] > nums[j+1]:
+        temp = nums[j]
+        nums[j] = nums[j+1]
+        nums[j+1] = temp
 
 
 nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
