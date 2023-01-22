914c4ba1650e1e9ec746d11e3b9ae15824e32a43
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-22 14:25:16 +0800

    Implement swapping condition when left is greater than current element

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,15 @@
 def insertion_sort(nums):
   # loop from 1 to 9
   for i in range(1, len(nums)):
+    # inner loop that starts at the index of outer loop and iterates to check if swapping is needed
+    j = i
+    # swapping condition
+    # left is bigger than current element
+    while nums[j - 1] > nums[j]: 
+      # swapping
+      # looks for entry in index j and puts in index j - 1 and simulataneously looks for entry in j - 1 and puts in index j
+      nums[j - 1], nums[j] = nums[j], nums[j -  1]
+
 
 nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
 insertion_sort(nums)
