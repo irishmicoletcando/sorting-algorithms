0f5bf5b5153de746f69ecc4bf81e6df71edf4c39
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-22 15:22:21 +0800

    Checks if left is less than right

--- quick_sort.py
+++ quick_sort.py
@@ -1,5 +1,7 @@
 def quick_sort(nums, left, right):
+  if left < right: # sub-array contains 2 elements
 
+  # if length of subarray = 1, do nothing
 
 nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
 quick_sort(nums)
