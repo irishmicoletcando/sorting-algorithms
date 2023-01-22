50fceda7a3a1977c74bbcab489c6ab57c58f2aa8
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-22 15:24:08 +0800

    Implement a function that returns index of pivot element

--- quick_sort.py
+++ quick_sort.py
@@ -1,7 +1,10 @@
 def quick_sort(nums, left, right):
   if left < right: # sub-array contains 2 elements
-
+      partition_pos = partition(nums, left, right)
   # if length of subarray = 1, do nothing
+
+
+def partition(nums, left, right): # returns the index of pivot element after the first step of quick sort
 
 nums = [53, 82, 9, 13, 98, 73, 18, 61, 16, 62]
 quick_sort(nums)
